f176b76b11ff847aba2993964777013f2f48f63b
mynamejeffee <76509820+mynamejeffee@users.noreply.github.com>
2020-12-23 20:07:22 +0800

    Separate file for displaying outputs

--- Queries.sql
+++ Queries.sql
@@ -41,18 +41,7 @@ INSERT INTO videoGame(game_title, developer, publisher, platform, game_status, r
 	'upcoming', 
 	'2020-12-10 00:00:00');
 */
-SELECT * FROM videogame;
 
--- ETO AY EXAMPLE NG SHOWING NG REMOVED GAMES!
-SELECT game_title as removed_games FROM videogame WHERE game_status = 'removed';
-
--- ETO AY EXAMPLE NG SHOWING NG RELEASED GAMES!
-SELECT game_title as available_games FROM videogame WHERE game_status = 'available';
-
--- yeshhh
--- yessssss
--- ETO AY EXAMPLE NG SHOWING NG UPCOMING GAMES!
-SELECT game_title as upcoming_games FROM videogame WHERE game_status = 'upcoming';
 
 
 
