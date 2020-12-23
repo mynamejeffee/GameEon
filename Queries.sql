d885a3b0364fbff7578486cba386ededc0a0c36b
mynamejeffee <76509820+mynamejeffee@users.noreply.github.com>
2020-12-23 19:46:24 +0800

    Testing

--- Queries.sql
+++ Queries.sql
@@ -1,9 +1,9 @@
---IM Project
+-- IM Project
 
 -- Creating the database
---CREATE DATABASE GameEon;
-USE GameEon;
-
+-- CREATE DATABASE GameEon;
+USE gameeon;
+/*
 -- Creating the tables
 -- Table for video game
 
@@ -14,8 +14,8 @@ CREATE TABLE videoGame (
 	publisher varchar(255),
 	platform varchar(255),
 	game_status varchar(255),
-	release_date TIMESTAMP,
-)
+	release_date TIMESTAMP
+);
 
 -- ETO AY EXAMPLE NG GAME!
 INSERT INTO videoGame(game_title, developer, publisher, platform, game_status, release_date) VALUES
@@ -40,7 +40,7 @@ INSERT INTO videoGame(game_title, developer, publisher, platform, game_status, r
 	'PC, Playstation', 
 	'upcoming', 
 	'2020-12-10 00:00:00');
-
+*/
 SELECT * FROM videogame;
 
 -- ETO AY EXAMPLE NG SHOWING NG REMOVED GAMES!
@@ -50,6 +50,7 @@ SELECT game_title as removed_games FROM videogame WHERE game_status = 'removed';
 SELECT game_title as available_games FROM videogame WHERE game_status = 'available';
 
 -- yes
+-- yessssss
 -- ETO AY EXAMPLE NG SHOWING NG UPCOMING GAMES!
 SELECT game_title as upcoming_games FROM videogame WHERE game_status = 'upcoming';
 
