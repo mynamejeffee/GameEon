7b18968a0c8a8904839c57affc5d51c700244f6e
mynamejeffee <76509820+mynamejeffee@users.noreply.github.com>
2020-12-23 20:13:24 +0800

    Added separate file for displaying outputs
    Separate file for displaying outputs only.

---
+++ Display Queries.sql
@@ -0,0 +1,12 @@
+SELECT * FROM videogame;
+
+-- ETO AY EXAMPLE NG SHOWING NG REMOVED GAMES!
+SELECT game_title as removed_games FROM videogame WHERE game_status = 'removed';
+
+-- ETO AY EXAMPLE NG SHOWING NG RELEASED GAMES!
+SELECT game_title as available_games FROM videogame WHERE game_status = 'available';
+
+-- yeshhh
+-- yessssss
+-- ETO AY EXAMPLE NG SHOWING NG UPCOMING GAMES!
+SELECT game_title as upcoming_games FROM videogame WHERE game_status = 'upcoming';
