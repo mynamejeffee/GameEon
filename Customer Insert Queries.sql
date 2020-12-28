d49833564441edd9cb08c0f9df2e13cc12f24f00
mynamejeffee <76509820+mynamejeffee@users.noreply.github.com>
2020-12-28 16:00:20 +0800

    Added Customer Insert.sql File
    Use this file to insert customers in the database.

---
+++ Customer Insert Queries.sql
@@ -0,0 +1,180 @@
+
+USE gameeon;
+-- Inserting customers
+INSERT INTO customer(username, e_mail, billing_info, country, region_state, city) VALUES
+(
+    'my_name_is_jeff',
+    'mynmjef@yahoo.com',
+    '5061211960616173',
+    'Philippines',
+    'Davao Region',
+    'Davao City'
+), (
+    'blahblahblah',
+    'bbb@gmail.com',
+    '6521460553598270',
+    'Germany',
+    'Berlin',
+    'Berlin'
+), (
+    'deutschland1999',
+    'deutschfurLeben@yahoo.de',
+    '3857644801037747',
+    'Germany',
+    'Bavaria',
+    'Munich'
+), (
+    'idontspeakgerman',
+    'idkdeutsch@yahoo.com',
+    '4562946840022090',
+    'United States of America',
+    'Texas',
+    'Abbott Hill'
+), (
+    'boots24/7',
+    'landminezzz@protonmail.com',
+    '2556841131113050',
+    'England',
+    'London',
+    'London'
+), (
+    'Mr.BrightSide',
+    'itwasonlyakiss@yahoo.com',
+    '3004545645310000',
+    'Vietnam',
+    'Tay Nguyen',
+    'Dong Son'
+), (
+    'IRONMAN005',
+    'mrstark1337@gmail.com',
+    '5610591081018250',
+    'United States of America',
+    'Florida',
+    'Jacksonville'
+), (
+    'bullFr0gs',
+    'ihopeugoweridont@yahoo.com',
+    '5105105235175100',
+    'United States of America',
+    'Florida',
+    'Jacksonville'
+), (
+    'siebenundsiebzig',
+    'handynummer@protonmail.de',
+    '4510645983016543',
+    'Germany',
+    'Hamburg',
+    'Hamburg'
+), (
+    'Marquis_deSade',
+    'writerprisoner@yahoo.fr',
+    '6221930324972270',
+    'France',
+    'Bretagne',
+    'Rennes'
+), (
+    'KelseyLloyd',
+    'lloyedkelsey@gmail.com',
+    '4390685183089703',
+    'Singapore',
+    'East Region',
+    'Pasir Ris'
+), (
+    'wastelanders',
+    'yourwasteland@gmail.com',
+    '4510645983016543',
+    'United States of America',
+    'Maryland',
+    'Baltimore'
+), (
+    'MotivesJohns',
+    'iftheaccidentwill@hotmail.com',
+    '2460697692880018',
+    'Australia',
+    'Queensland',
+    'Brisbane'
+), (
+    'horrorGamer555',
+    'edwardmickey@protonmail.com',
+    '5061046402408866',
+    'Australia',
+    'Victoria',
+    'Melbourne'
+), (
+    'aPairof_Sunglasses',
+    'asmuchofthesky@gmail.com',
+    '5019593633426863',
+    'Mexico',
+    'Mexico City',
+    'Mexico City'
+), (
+    'taylor_Swift<3<3',
+    'loveyoutay@hotmail.com',
+    '6521691800069984',
+    'Egypt',
+    'Cairo',
+    'Cairo'
+), (
+    'Oscar_Amalfitano',
+    'voicesinmyhead@gmail.com',
+    '8863123038379698',
+    'Mexico',
+    'Sonora',
+    'Santa Teresa'
+), (
+    'OscarFate1993',
+    'thejournalist@protonmail.com',
+    '5061921463511073',
+    'United States of America',
+    'New Jersey',
+    'Trenton'
+), (
+    'ebonydarknessway',
+    'goffikgirlz@gmail.com',
+    '2451311205656987',
+    'England',
+    'London',
+    'London'
+), (
+    'oranges',
+    'orangen@yahoo.com.eu',
+    '4017695078506219',
+    'United States of America',
+    'Ohio',
+    'Columbus'
+), (
+    'ghostFan',
+    'ghost4life@hotmail.com',
+    '3553111243260601',
+    'Sweden',
+    'Gotaland',
+    'Dalsland'
+), (
+    'yes_yes_yo',
+    'jajaja@outlook.com',
+    '3572016894682481',
+    'Philippines',
+    'Manila',
+    'Quezon City'
+), (
+    'Never_ever_land',
+    'nevereverland@yahoo.com.jp',
+    '3563319012708381',
+    'Japan',
+    'Shikoku',
+    'Tokushima'
+), (
+    'My_Body_a_Cage',
+    'kipsmefromdans@gmail.com',
+    '3095018772985570',
+    'United States of America',
+    'New Jersey',
+    'Newark'
+), (
+    'dead-Language',
+    'monumentalz@gmail.com',
+    '6390293480987771',
+    'China',
+    'Hong Kong',
+    'Kwun Tong'
+);
